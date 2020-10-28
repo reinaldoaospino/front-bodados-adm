41d4ff1d0dce3e07403354c67f3155bd7e638430
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-10-28 15:59:56 -0300

    Update product-service.js

--- src/services/product/product-service.js
+++ src/services/product/product-service.js
@@ -24,14 +24,49 @@ export const CreateProduct = async (productData) => {
     return response.status;
 }
 
-export const GetProducts = async()=>{
+export const GetProducts = async () => {
     var token = await GetToken();
 
-    const response = await bocadosApi.get('/api/product',{
+    const response = await bocadosApi.get('/api/product', {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     })
 
     return response.data;
+}
+
+export const UpdateProducts = async (productData) => {
+    var tokenTask = GetToken();
+
+    const data = JSON.stringify({
+        "ProductName": productData.productName,
+        "Price": productData.price,
+        "FeaturedProduct": productData.featuredProduct,
+        "Category": productData.category,
+    })
+
+    var token = await tokenTask;
+
+    const response = await bocadosApi.post('/api/product', data, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response.status;
+}
+
+export const DeleteProduct = async (id) => {
+    var tokenTask = GetToken();
+
+    var token = await tokenTask;
+
+    const response = await bocadosApi.post('/api/product/' + id, data, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response.status;
 }
