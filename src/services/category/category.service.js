86df6177754ca640ec60397dd93184f6a08f8db8
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-10-29 11:32:40 -0300

    Create category.service.js

---
+++ src/services/category/category.service.js
@@ -0,0 +1,65 @@
+import bocadosApi from '../../apis/bocadosApis';
+import { GetToken } from '../token/token-service';
+const endPoint = '/api/category';
+
+export const CreateCategory = async (categoryData) => {
+    var tokenTask = GetToken();
+
+    const data = JSON.stringify({
+        "CategoryName": categoryData.categoryName
+    })
+
+    var token = await tokenTask;
+
+    const response = await bocadosApi.post(endPoint, data, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response.status;
+}
+
+export const GetPCategory = async () => {
+    var token = await GetToken();
+
+    const response = await bocadosApi.get(endPoint, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response.data;
+}
+
+export const UpdatePCategory = async (categoryData) => {
+    var tokenTask = GetToken();
+
+    const data = JSON.stringify({
+        "CategoryName": categoryData.categoryName
+    })
+
+    var token = await tokenTask;
+
+    const response = await bocadosApi.put(endPoint, data, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response.status;
+}
+
+export const DeleteCategory = async (id) => {
+    var tokenTask = GetToken();
+
+    var token = await tokenTask;
+
+    const response = await bocadosApi.delete(`${endPoint}/${id}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response.status;
+}
